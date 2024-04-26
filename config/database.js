7fb1ea1c58f9a206235baa456effa85d77c69992
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-26 18:47:39 +0530

    main

--- config/database.js
+++ config/database.js
@@ -2,5 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = () =>{
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+    })
 }
