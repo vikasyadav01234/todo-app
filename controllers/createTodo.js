c15cb708271c4d32cdb66457196aad084e21977c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-28 11:24:02 +0530

    main

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -10,7 +10,12 @@ exports.createTodo = async(req,res) => {
         //create a new Todo Obj and insert in DB
         const response = await Todo.create({title,description});
 
-        
+        //send a json response with a success flag 
+        res.status(200).json(
+            {
+                
+            }
+        )
     }
     catch(err){
 
