22cf5fa0eb8ab13f58e6d78a8970dc3293a5ab37
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-28 08:54:06 +0530

    main

--- models/Todo.js
+++ models/Todo.js
@@ -5,6 +5,22 @@ const toduSchema = new mongoose.Schema(
         title:{
             type:String,
             required:true,
+            maxLength:50,
+        },
+        description:{
+            type:String,
+            required:true,
+            maxLength:50,
+        }
+        createdAt:{
+            type:Date,
+            required:true,
+            default:Date.now(),
+        }
+        updateAt:{
+            type:Date,
+            required:true,
+            default:Date.now(),
         }
     }
 )
