c366fbc309b560eca4a1608443e660f1f28f301e
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-28 11:15:34 +0530

    main

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -7,6 +7,7 @@ exports.createTodo = async(req,res) => {
     try{
         //extract title and description from request body
         const {title,description} = req.body;
+        //create a new Todo Obj and insert
     }
     catch(err){
 
