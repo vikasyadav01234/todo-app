6da8f1039c2842471df7da63f7558a0393bfe640
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-29 10:26:14 +0530

    main

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 //import routes for TODO API
 
-const todoRoutes = require("./routes")
+const todoRoutes = require("./routes/todos")
