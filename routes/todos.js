f39336080e929a424dfccd9efd5d99b38bb885c1
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-29 10:10:10 +0530

    main

--- routes/todos.js
+++ routes/todos.js
@@ -2,4 +2,4 @@ const express = require("express");
 const router = express.router();
 
 //import controller
-const {createTodo} = require("../controllers")
+const {createTodo} = require("../controllers/createTodo")
