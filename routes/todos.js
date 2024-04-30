6a7c7d59083d02b6c405ada0dce0549f03ffbb38
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-30 10:58:43 +0530

    main

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //import controller
 const {createTodo} = require("../controllers/createTodo");
-const {getTodo} = require("../controllers/getTodo");
+const {getTodo, getTodoById} = require("../controllers/getTodo");
 //define API routes
 router.post("/createTodo",createTodo);
 router.get("/getTodos", getTodo);
