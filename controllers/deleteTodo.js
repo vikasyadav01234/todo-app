af2534520d8b821d4ee49ed42798f50a6cf6b254
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-30 23:19:35 +0530

    main

---
+++ controllers/deleteTodo.js
@@ -0,0 +1,13 @@
+//import th Model
+const Todo = require("../models/Todo");
+
+//define route handler
+
+exports.deleteTodo = async(req,res) => {
+    try{
+       
+    }
+    catch(err){
+       
+    }
+}
