8d35f33cf23bf916460a4416487cbd0ac4298984
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-30 10:22:01 +0530

    main

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -5,7 +5,7 @@ const Todo = require("../models/Todo");
 exports.getTodo = async(req,res) => {
     try{
         //fetch all todo items from database
-        const todos = await Todo.find({})
+        const todos = await Todo.find({});
     }
     catch(err){
         
